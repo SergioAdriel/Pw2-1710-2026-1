806e120884dd6faff99bebe868c72af68a9de564
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2025-09-02 09:47:48 -0600

    Clase del 02092025

---
+++ 02092025/app.js
@@ -0,0 +1,18 @@
+// function numeroRandom(min, max) {
+//   return Math.floor(Math.random() * (max - min) + min);
+// }
+
+// console.log(numeroRandom(1, 10)); // Genero un numero entre 1 y 10 random
+
+// let numeroAdivinado = parseInt(prompt("Adivina el numero secreto"))
+// let numeroVidas = 3;
+// while(numeroRandom !== numeroAdivinado && numeroVidas > 1){  // Si el numero random y el numero de vidas es mayor a 1 sigues jugando
+//     numeroVidas --;
+//     numeroAdivinado = parseInt(prompt("Te quedan todavia" + numeroVidas))
+// }
+
+// if (numeroAdivinado == numeroRandom){
+//     alert("Ganaste");
+// } else{
+//     alert("Perdiste")
+// }
---
+++ 02092025/app2.js
@@ -0,0 +1,11 @@
+let frutas = ["kiwis","manzana","platano","pera"];
+// console.log(frutas);
+// console.log(frutas.length);
+// console.log(frutas[0]);
+// console.log(frutas[1]);
+// console.log(frutas[2]);
+// console.log(frutas[3]);
+
+for( let i = 0; i < frutas.length; i ++){
+    console.log(frutas[i]);
+}
