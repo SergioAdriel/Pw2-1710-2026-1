25b6c581b9c456386921d389a2a068bb9b098c5d
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2025-09-04 10:08:19 -0600

    Operaciones Aritmeticas

--- Clases/04092025/app3.js
+++ Clases/04092025/app3.js
@@ -1,9 +1,25 @@
 function sumar (n1,n2){
     return parseInt(n1)+parseInt(n2);
 }
+function resta (n1,n2){
+    return parseInt(n1)-parseInt(n2);
+}
+function multiplicacion (n1,n2){
+    return parseInt(n1)*parseInt(n2);
+}
+function division (n1,n2){
+    return parseInt(n1)/parseInt(n2);
+}
+var numero1 = prompt("Ingresa el Primer Numero:");
+var numero2 = prompt("Ingresa el Segundo Numero:");
 
-let numero1 = prompt("Ingresa el Primer Numero:");
-let numero2 = prompt("Ingresa el Segundo Numero:");
+var resultadoSuma = sumar(numero1,numero2);
+var resultadoResta = resta(12,35);
+var resultadoMultiplicacion = multiplicacion(239,3290);
+var resultadoDivision = division(230,21);
 
-let resultado = sumar(numero1,numero2);
-console.log(resultado);
+console.log(resultadoSuma);
+console.log(resultadoResta);
+console.log(resultadoMultiplicacion);
+console.log(resultadoDivision);
+
