4faa037f094a85a87b0e202c6dce0c1a7fef520b
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2025-09-11 09:33:42 -0600

    Actualizando Clase del 11092025

--- Clases/11092025/app.js
+++ Clases/11092025/app.js
@@ -1,14 +1,27 @@
+// Arreglo vacio
 let nombres = [""];
 
+// Agregar nombre al arreglo
 function agregarNombre(){
     let nombre = prompt("Agrega el nombre: ")
     if(nombre){
         nombres.push
         alert(`Nombre ${nombre} agregado exitosamente`)
     }else{
-
+        alert(`El nombre esta vacio`)
     }
 }
 
+function mostrarNombres{
+    if(nombres.length===0){
+        alert(`Arreglo Vacio`);
+    }else{
+        let mensaje = "Nombres almacenados: \n"
+        nombres.forEach((nombre, i)=> {
+            mensaje += `${i+1}. ${nombre}\n`; // Al indice se le suma 1 
+        }); //Nombre va a ir aterando en el arreglo
+        alert(mensaje); // Imprimiria Nombres almacenados: i nombre
+    }
+}
 
 
