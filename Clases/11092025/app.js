d438aad72570ad86fc18084f330e7d3f381bed71
SergioAdriel <sergiomunoz320@aragon.unam.mx>
2025-09-11 09:47:32 -0600

    Actualizando Clase del 11092025

--- Clases/11092025/app.js
+++ Clases/11092025/app.js
@@ -6,22 +6,40 @@ function agregarNombre(){
     let nombre = prompt("Agrega el nombre: ")
     if(nombre){
         nombres.push
-        alert(`Nombre ${nombre} agregado exitosamente`)
+        alert(`El nombre ${nombre} agregado exitosamente`)
     }else{
         alert(`El nombre esta vacio`)
     }
 }
 
-function mostrarNombres{
+// Muestra nombres del arreglo
+function mostrarNombres(){
     if(nombres.length===0){
         alert(`Arreglo Vacio`);
     }else{
         let mensaje = "Nombres almacenados: \n"
-        nombres.forEach((nombre, i)=> {
-            mensaje += `${i+1}. ${nombre}\n`; // Al indice se le suma 1 
-        }); //Nombre va a ir aterando en el arreglo
+        // nombres.forEach((nombre, i)=> {
+        //     mensaje += `${i+1}. ${nombre}\n`; // Al indice se le suma 1 
+        // }); //Nombre va a ir aterando en el arreglo
+        for (let i = 0; i < nombres.length; i++) {
+            mensaje += `${i + 1}. ${nombres[i]}\n`;
         alert(mensaje); // Imprimiria Nombres almacenados: i nombre
     }
 }
 
+// Iniciar programa
+mostrarMenu(){
+    let opcion;
 
+    do{
+        opcion=prompt(`
+            Seleccione una opcion
+            1. Agregar Nombre
+            2. Imprimir nombres
+            3. Salir
+            `)
+    }
+}
+
+// Iniciar programa
+mostrarMenu();
